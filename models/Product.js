61464a5054bd25eb7bcc584177d679da6c1255ef
G1oka <gioka018@gmail.com>
2024-11-12 03:32:55 +0400

    update

--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,7 @@ const productSchema = new mongoose.Schema({
         type: String 
     },
     images: [String],
-    Category: {
+    category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
     },
