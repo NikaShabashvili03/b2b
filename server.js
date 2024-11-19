c661213271bc8b298157634c714fb1640568754f
G1oka <gioka018@gmail.com>
2024-11-19 21:38:42 +0400

    update cors

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.options('*', cors());
-// Routes
+
 app.use('/api/v1', v1Router)
 
 
