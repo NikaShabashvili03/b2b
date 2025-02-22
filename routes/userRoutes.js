731e027356649ad2386f619884bf5c841e478ed5
G1oka <gioka018@gmail.com>
2025-02-23 00:29:32 +0400

    A

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../utils/checkUser'); 
+const auth = require('../utils/checkAdmin');
+
 
 // User Routes
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.get('/profile', auth.checkUser, userController.profile);
-router.get('/all', auth, userController.getAllUsers);
+router.get('/all', auth.checkAdmin, userController.getAllUsers);
 
 module.exports = router;
